ebbeb2d4de1af96c68bdd63e682021e24205a212
PaavanVyas <paavan.vyas@invennico.com>
2025-03-03 11:33:07 +0530

    Share button done

--- view_blog.php
+++ view_blog.php
@@ -75,7 +75,30 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['blog_id'])) {
     <?php if (!empty($blog_cover)) { ?>
         <img src="<?php echo htmlspecialchars($blog_cover); ?>" class="card-img-top" alt="blog cover">
         <?php } ?>
-        <h4><?php echo htmlspecialchars($blog_title); ?></h4>
+        <div class="d-flex justify-content-between mt-2">
+            <h4><?php echo htmlspecialchars($blog_title); ?></h4>
+        </div>
+        
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Share Link</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <h6>Copy Current URL:</h6>
+        <p id="urlText"></p> <!-- Display URL here -->
+        <button id="copyBtn" class="btn btn-primary">Copy</button>
+    </div>
+
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+        
         <p><b>By: <?php echo htmlspecialchars($username); ?></b></p>
         <p>Category: <?php echo htmlspecialchars($blog_category);?></p>
         <p>Posted on: <?php echo htmlspecialchars($datecreated);?></p>
@@ -135,9 +158,39 @@ echo $content;
 </div>
 
 <div class="container border div-design-comment">
+
+<?php
+            if (isset($user_id)){
+            if ($blog_user_id != $user_id)
+            {
+            ?>
+            <div class=" container mt-2 border div-design-add-comment">
+                <h4>Add a Comment</h4>
+                <form action="create_comment.php" method="POST" id="replydata">
+
+                    <div class="mb-1 form-group ">
+                        
+                        <textarea type="comment_content" id="comment_content" name="comment_content" class="form-control" required ></textarea>
+                        <input type="hidden" value="<?php echo $user_id; ?>" id="user_id" name="user_id">
+                        <input type="hidden" value="<?php echo $blog_id; ?>" id="blog_id" name="blog_id">
+                        <input type="hidden" value="<?php echo $username; ?>" id="username" name="username">
+                    </div>
+                    <div>
+                        <center><input type="submit" value="Add Comment" class = "mb-2"></center>
+                    </div>
+                </form>
+            </div>
+
+            <?php
+                }
+            }
+            ?>
+
+            
             <h4>Comments</h4>
             
             <div class="container mt-3 comment-border">  
+            
             <?php
                 $stmt_comment = $conn->prepare("SELECT * FROM comments_data JOIN users ON comments_data.user_id = users.user_id WHERE blog_id = ?");
                 $stmt_comment->bind_param("s", $blog_id);
@@ -202,32 +255,7 @@ echo $content;
 
             </div>
             </div>
-            <?php
-            if ($blog_user_id != $user_id)
-            {
-            ?>
-            <div class=" container mt-2 border div-design-add-comment">
-                <h4>Add a Comment</h4>
-                <form action="create_comment.php" method="POST" id="replydata">
-
-                    <div class="mb-1 form-group ">
-                        
-                        <textarea type="comment_content" id="comment_content" name="comment_content" class="form-control" required ></textarea>
-                        <input type="hidden" value="<?php echo $user_id; ?>" id="user_id" name="user_id">
-                        <input type="hidden" value="<?php echo $blog_id; ?>" id="blog_id" name="blog_id">
-                        <input type="hidden" value="<?php echo $username; ?>" id="username" name="username">
-                    </div>
-                    <div>
-                        <center><input type="submit" value="Add Comment" class = "mb-2"></center>
-                    </div>
-                </form>
-            </div>
-            </div>
-            <h4>HEllo this is in new-branch</h4>
-            <?php
-                }
-            ?>
-
+            
 <?php
 
 $stmt->close();
