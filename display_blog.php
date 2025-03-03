d977f744ef7e0584cb9e0dcf3cc24b54f2db4225
PaavanVyas <paavan.vyas@invennico.com>
2025-03-03 17:44:16 +0530

    Fixed some design

--- display_blog.php
+++ display_blog.php
@@ -18,7 +18,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"/>
     <link rel="stylesheet" href="style.css"/>
 </head>
-<body class="bg-color-card">
+<body>
 <nav class="navbar navbar-expand-lg">
   <div class="container-fluid">
   <a class="navbar-brand" href="index.php?user_id=<?php echo htmlspecialchars($user_id); ?>">
