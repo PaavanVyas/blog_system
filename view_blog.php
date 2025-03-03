55703799aa1aba3fa4acf083838641f1c7fd51c2
PaavanVyas <paavan.vyas@invennico.com>
2025-03-03 11:33:07 +0530

    Share button done

--- view_blog.php
+++ view_blog.php
@@ -80,11 +80,11 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['blog_id'])) {
         <?php } ?>
         <div class="d-flex justify-content-between mt-2">
             <h4><?php echo htmlspecialchars($blog_title); ?></h4>
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Share</button>
         </div>
+        
         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
+        <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
       <div class="modal-header">
         <h1 class="modal-title fs-5" id="exampleModalLabel">Share Link</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
@@ -161,9 +161,39 @@ echo $content;
 </div>
 
 <div class="container border div-design-comment">
+
+<?php
+            if (isset($user_id)){
+            if ($blog_user_id != $user_id)
+            {
+            ?>
+            <div class=" container mt-2 border div-design-add-comment">
+                <h4>Add a Comment</h4>
+                <form action="create_comment.php" method="POST" id="replydata">
+
+                    <div class="mb-1 form-group ">
+                        
+                        <textarea type="comment_content" id="comment_content" name="comment_content" class="form-control" required ></textarea>
+                        <input type="hidden" value="<?php echo $user_id; ?>" id="user_id" name="user_id">
+                        <input type="hidden" value="<?php echo $blog_id; ?>" id="blog_id" name="blog_id">
+                        <input type="hidden" value="<?php echo $username; ?>" id="username" name="username">
+                    </div>
+                    <div>
+                        <center><input type="submit" value="Add Comment" class = "mb-2"></center>
+                    </div>
+                </form>
+            </div>
+
+            <?php
+                }
+            }
+            ?>
+
+            
             <h4>Comments</h4>
             
             <div class="container mt-3 comment-border">  
+            
             <?php
                 $stmt_comment = $conn->prepare("SELECT * FROM comments_data JOIN users ON comments_data.user_id = users.user_id WHERE blog_id = ?");
                 $stmt_comment->bind_param("s", $blog_id);
@@ -226,33 +256,7 @@ echo $content;
 
             </div>
             </div>
-            <?php
-            if (isset($user_id)){
-            if ($blog_user_id != $user_id)
-            {
-            ?>
-            <div class=" container mt-2 border div-design-add-comment">
-                <h4>Add a Comment</h4>
-                <form action="create_comment.php" method="POST" id="replydata">
-
-                    <div class="mb-1 form-group ">
-                        
-                        <textarea type="comment_content" id="comment_content" name="comment_content" class="form-control" required ></textarea>
-                        <input type="hidden" value="<?php echo $user_id; ?>" id="user_id" name="user_id">
-                        <input type="hidden" value="<?php echo $blog_id; ?>" id="blog_id" name="blog_id">
-                        <input type="hidden" value="<?php echo $username; ?>" id="username" name="username">
-                    </div>
-                    <div>
-                        <center><input type="submit" value="Add Comment" class = "mb-2"></center>
-                    </div>
-                </form>
-            </div>
-            </div>
-            <?php
-                }
-            }
-            ?>
-
+            
 <?php
 
 $stmt->close();
